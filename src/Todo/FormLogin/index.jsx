eab9e50a6a4a88a2973cd04088165c7107b7497f
nguyentrungphap <phapnguyen05032002@gmail.com>
2024-08-06 09:51:33 +0700

    change(login):validator login form

--- src/Todo/FormLogin/index.jsx
+++ src/Todo/FormLogin/index.jsx
@@ -1,53 +1,71 @@
+import { useState } from 'react';
 import validator from 'validator';
-import { Toaster, toast } from 'alert';
 
 function FormLogin() {
-
-    const validateEmail = (value) => {
-        if (validator.isEmail(value)) {
-            toast.success('Email hợp lệ');
-        } else {
-            toast.error('Email không hợp lệ');
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [errors, setErrors] = useState({
+        email: '',
+        password: ''
+    });
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (validator.isEmail(email)) { 
+            setErrors(prevErrors => ({ ...prevErrors, email: null }));
+        } 
+        else {
+            setErrors(prevErrors => ({ ...prevErrors, email: 'Email không hợp lệ' }));
         }
-    };
-    const validatePassword = (value) => {
+
         const minLength = /.{8,}/;
         const upperCase = /[A-Z]/;
         const number = /[0-9]/;
         const specialChar = /[!@#$%^&*(),.?":{}|<>]/;
 
-        if (!minLength.test(value)) {
-            toast.error('Mật khẩu phải có ít nhất 8 ký tự');
-        } else if (!upperCase.test(value)) {
-            toast.error('Mật khẩu phải có ít nhất một chữ cái viết hoa');
-        } else if (!number.test(value)) {
-            toast.error('Mật khẩu phải có ít nhất một số');
-        } else if (!specialChar.test(value)) {
-            toast.error('Mật khẩu phải có ít nhất một ký tự đặc biệt');
+        if (!minLength.test(password)) {
+            setErrors(prevErrors => ({ ...prevErrors, password: 'Mật khẩu phải có ít nhất 8 ký tự' }));
+        } else if (!upperCase.test(password)) {
+            setErrors(prevErrors => ({ ...prevErrors, password: 'Mật khẩu phải có ít nhất một chữ cái viết hoa' }));
+        } else if (!number.test(password)) {
+            setErrors(prevErrors => ({ ...prevErrors, password: 'Mật khẩu phải có ít nhất một số' }));
+        } else if (!specialChar.test(password)) {
+            setErrors(prevErrors => ({ ...prevErrors, password: 'Mật khẩu phải có ít nhất một ký tự đặc biệt' }));
         } else {
-            toast.success('Mật khẩu hợp lệ');
+            setErrors(prevErrors => ({ ...prevErrors, password: null }));
         }
     };
+    console.log(errors)
 
     return (
-        <form action="">
-            <h1>Form Login</h1>
-            <div>
-                <div>
-                    <label htmlFor="Email">Email:</label>
+        <form onSubmit={handleSubmit} className='bg-gray-600 w-max p-12 m-auto grid gap-10'>
+            <h1 className='text-white font-bold text-6xl'>Form Login</h1>
+            <div className='grid gap-3'>
+                <div className='flex flex-col items-start grid gap-2'>
+                    <label htmlFor="Email" className='text-white text-start'>Email:</label>
                     <input
                         type="text"
-                        onBlur={(e) => validateEmail(e.target.value)}
+                        onChange={(e) => setEmail(e.target.value)}
                         placeholder="Nhập email"
+                        className='w-full'
                     />
+                    <p className={errors.email === 'Email hợp lệ' ? 'valid' : 'invalid'}>
+                        {errors.email}
+                    </p>
                 </div>
-                <div>
-                    <label htmlFor="Password">Password</label>
-                    <input type="password" onBlur={(e) => validatePassword(e.target.value)} />
-                    
+                <div className='flex flex-col items-start grid gap-2'>
+                    <label htmlFor="Password" className='text-white text-start'>Password</label>
+                    <input
+                        type="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Nhập mật khẩu"
+                        className='w-full'
+                    />
+                    <p className={errors.password === 'Mật khẩu hợp lệ' ? 'valid' : 'invalid'}>
+                        {errors.password}
+                    </p>
                 </div>
             </div>
-            <Toaster />
+            <button type="submit" className='bg-red-600 text-white w-max px-6 py-2 m-auto'>Submit</button>
         </form>
     );
 }
